dd89dce4b3e3383c05829f02c1861db4394daa9c
landrozz <leandrozanobine@gmail.com>
2022-12-15 13:38:11 -0300

    revisando e enviando ao Github

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let tabuada = Number(prompt("escolha um numero para fazer a tabuada"))
+
+for(let i = 1; i <= 10; i++){
+    
+    console.log(tabuada * i)
+    
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,5 @@
+maioresDesertos = ['Saara','Arabia','Gobi','Patagonia','Grande-Bacia']
+console.log('Os maiores desertos do mundo:')
+for(let i = 0; i !== maioresDesertos.length ; i++){
+    console.log(`${i+1} - ${maioresDesertos[i]}`)
+}
